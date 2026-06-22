49cc918796d7859f0f7a2c1004d07e6ecb7b50b6
inq <gofiri@gmail.com>
2026-06-23 02:00:34 +0700

    support intel

--- pg/build.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    let arch = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap_or_default();
-    if arch != "aarch64" && arch != "arm64" {
-        panic!("cartel-pg supports only ARM64/AArch64 targets; got target_arch={arch}",);
-    }
-}
